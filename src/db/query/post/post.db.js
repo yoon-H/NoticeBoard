9b2d2d03696caa1ce17f802d396c8065f28852bd
yoon-H <ucando0320@gmail.com>
2025-05-15 01:40:43 +0900

    add : post.db.js

---
+++ src/db/query/post/post.db.js
@@ -0,0 +1,26 @@
+import pool from "../../database.js";
+import SQL_POST_QUERIES from "./post.queries.js";
+
+export const getAllPosts = async () => {
+  const posts = await pool.query(SQL_POST_QUERIES.GET_ALL_POSTS);
+
+  return posts;
+};
+
+export const createPost = async (title, author, content) => {
+  await pool.query(SQL_POST_QUERIES.CREATE_POST, [title, author, content]);
+};
+
+export const getPost = async (id) => {
+  const post = await pool.query(SQL_POST_QUERIES.GET_POST, [id]);
+
+  return post;
+};
+
+export const editPost = async (title, content, id) => {
+  await pool.query(SQL_POST_QUERIES.EDIT_POST, [title, content, id]);
+};
+
+export const deletePost = async (id) => {
+  await pool.query(SQL_POST_QUERIES.DELETE_POST, [id]);
+};
