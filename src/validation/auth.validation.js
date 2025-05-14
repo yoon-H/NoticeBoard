61eecafae2686cdf02d4d2b3b1ee3c1cc43550a3
yoon-H <ucando0320@gmail.com>
2025-05-14 15:12:51 +0900

    fix : export validations

---
+++ src/validation/auth.validation.js
@@ -0,0 +1,5 @@
+export const ID_REG = /^[a-z][a-z0-9]{3,19}/;
+export const PW_REG =
+  /^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[+=%_!@#$^&*?])[A-Za-z0-9+=%_!@#$^&*?]{8,}$/;
+export const NAME_REG = /^[가-힣a-zA-Z0-9_-]{2,12}$/;
+
