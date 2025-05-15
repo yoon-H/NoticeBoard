4927cd87adc8e14e6d794386fcbaf69175439717
yoon-H <ucando0320@gmail.com>
2025-05-15 11:57:19 +0900

    fix : rename delete comment query

--- src/db/query/comment/comment.queries.js
+++ src/db/query/comment/comment.queries.js
@@ -4,7 +4,7 @@ const SQL_COMMENT_QUERIES = {
   CREATE_COMMENT:
     "INSERT INTO comments (author, content, post_id) VALUES (?,?,?)",
   EDIT_COMMENT: "UPDATE comments SET content = ? WHERE id = ? AND author = ?",
-  DELETE_POST: "DELETE FROM comments WHERE id = ? AND author = ?",
+  DELETE_COMMENT: "DELETE FROM comments WHERE id = ? AND author = ?",
 };
 
 export default SQL_COMMENT_QUERIES;
