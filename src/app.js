61e43583917bfe4f250e3f5126cf63dca3b14ebf
yoon-H <ucando0320@gmail.com>
2025-05-16 01:13:24 +0900

    feat : errorHandlerMiddleware

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import express from "express";
 import { config } from "./config/config.js";
+
+import errorHandlerMiddleware from "./middlewares/error-handler.middleware.js";
+
 import authRoutes from "./routes/auth.routes.js";
 import postRoutes from "./routes/post.routes.js";
 import commentRoutes from "./routes/comment.routes.js";
@@ -12,6 +15,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", [authRoutes, postRoutes, commentRoutes]);
+
+app.use(errorHandlerMiddleware);
 
 app.listen(PORT, HOST, () => {
   console.log(HOST, PORT, "서버가 열렸습니다.");
