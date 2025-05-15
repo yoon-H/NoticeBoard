5ec37ed3fbdbdd079459770dd5ea455fb3401f9a
yoon-H <ucando0320@gmail.com>
2025-05-15 11:18:07 +0900

    feat : add try catch in router

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -6,38 +6,48 @@ import { createUser, findUserByLoginId } from "../db/query/user/user.db.js";
 const router = Router();
 
 // 회원가입 API
-router.post("/auth/signup", async (req, res) => {
-  const { id, password, confirmPassword, name } = req.body;
+router.post("/auth/signup", async (req, res, next) => {
+  try {
+    const { id, password, confirmPassword, name } = req.body;
 
-  // 유효성 검사
-  if (!config.reg.id_reg.test(id)) new Error("아이디가 적합하지 않습니다.");
-  if (!config.reg.pw_reg.test(password))
-    new Error("비밀번호를 다시 입력해주세요.");
-  if (!config.reg.pw_reg.test(name)) new Error("유효하지 않는 아이디입니다.");
-  if (password === confirmPassword)
-    new Error("비밀번호와 확인 비밀번호가 일치하지 않습니다.");
+    // 유효성 검사
+    if (!config.reg.id_reg.test(id)) new Error("아이디가 적합하지 않습니다.");
+    if (!config.reg.pw_reg.test(password))
+      new Error("비밀번호를 다시 입력해주세요.");
+    if (!config.reg.pw_reg.test(name)) new Error("유효하지 않는 아이디입니다.");
+    if (password === confirmPassword)
+      new Error("비밀번호와 확인 비밀번호가 일치하지 않습니다.");
 
-  // 비밀번호 해싱
-  const hashedPw = bcrypt.hash(password, 10);
+    // 비밀번호 해싱
+    const hashedPw = bcrypt.hash(password, 10);
 
-  // 데이터 저장
-  await createUser(name, id, hashedPw);
-  return res.status(201).json({ message: "회원가입이 완료되었습니다." });
+    // 데이터 저장
+    await createUser(name, id, hashedPw);
+    return res.status(201).json({ message: "회원가입이 완료되었습니다." });
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.post("/auth/login", async (req, res) => {
-  const { id, password } = req.body;
+router.post("/auth/login", async (req, res, next) => {
+  try {
+    const { id, password } = req.body;
 
-  // 가입된 사용자 확인
-  const user = await findUserByLoginId(id);
+    // 가입된 사용자 확인
+    const user = await findUserByLoginId(id);
 
-  if (!user)
-    return res.status(400).json({ message: "유저 정보가 존재하지 않습니다." });
+    if (!user)
+      return res
+        .status(400)
+        .json({ message: "유저 정보가 존재하지 않습니다." });
 
-  if (!bcrypt.compare(password, user.password))
-    return res.status(400).json({ message: "비밀번호가 일치하지 않습니다." });
+    if (!bcrypt.compare(password, user.password))
+      return res.status(400).json({ message: "비밀번호가 일치하지 않습니다." });
 
-  return res.status(200).json({ message: "로그인이 완료되었습니다." });
+    return res.status(200).json({ message: "로그인이 완료되었습니다." });
+  } catch (err) {
+    next(err);
+  }
 });
 
 export default router;
--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -9,44 +9,64 @@ import {
 
 const router = Router();
 
-router.get("/posts", async (req, res) => {
-  const posts = await getAllPosts();
+router.get("/posts", async (req, res, next) => {
+  try {
+    const posts = await getAllPosts();
 
-  return res.status(200).json(posts);
+    return res.status(200).json(posts);
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.post("/posts", async (req, res) => {
-  const { title, author, content } = req.body;
+router.post("/posts", async (req, res, next) => {
+  try {
+    const { title, author, content } = req.body;
 
-  await createPost(title, author, content);
+    await createPost(title, author, content);
 
-  return res.status(200).json({ message: "게시글이 저장되었습니다." });
+    return res.status(201).json({ message: "게시글이 저장되었습니다." });
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.get("/posts/:postId", async (req, res) => {
-  const postId = req.params.postId;
+router.get("/posts/:postId", async (req, res, next) => {
+  try {
+    const postId = req.params.postId;
 
-  const post = await getPost(postId);
+    const post = await getPost(postId);
 
-  return res.status(200).json(post);
+    return res.status(200).json(post);
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.put("/posts/:postId", async (req, res) => {
-  const postId = req.params.postId;
-  const { title, content, id } = req.body;
+router.put("/posts/:postId", async (req, res, next) => {
+  try {
+    const postId = req.params.postId;
+    const { title, content, id } = req.body;
 
-  await editPost(title, content, postId, id);
+    await editPost(title, content, postId, id);
 
-  return res.status(200).json({ message: "게시글이 수정되었습니다." });
+    return res.status(200).json({ message: "게시글이 수정되었습니다." });
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.delete("/posts/:postId", async (req, res) => {
-  const postId = req.params.postId;
-  const { id } = req.body;
+router.delete("/posts/:postId", async (req, res, next) => {
+  try {
+    const postId = req.params.postId;
+    const { id } = req.body;
 
-  await deletePost(postId, id);
+    await deletePost(postId, id);
 
-  return res.status(200).json({ message: "게시글이 수정되었습니다." });
+    return res.status(200).json({ message: "게시글이 수정되었습니다." });
+  } catch (err) {
+    next(err);
+  }
 });
 
 export default router;
