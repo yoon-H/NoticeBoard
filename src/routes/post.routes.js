7e5978afab4579a189a6477aa70e8b223da1ab40
yoon-H <ucando0320@gmail.com>
2025-05-16 11:18:02 +0900

    feat: add post api in swagger ui

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -9,6 +9,39 @@ import {
 
 const router = Router();
 
+/**
+ * @swagger
+ * /api/posts:
+ *   get:
+ *     summary: "게시글 전체 조회"
+ *     description: "서버에 Get 방식으로 게시글 전체 조회 요청"
+ *     tags: [Post]
+ *     responses:
+ *       200:
+ *         description: 성공 메시지
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: number
+ *                     example: 1
+ *                   title:
+ *                     type: string
+ *                     example: 첫 번째 게시글
+ *                   name:
+ *                     type: string
+ *                     example: cool_user
+ *                   content:
+ *                     type: string
+ *                     example: 게시글 내용입니다.
+ *                   time:
+ *                     type: string
+ *                     example: 2025-05-15T15:23:05.000Z
+ */
 router.get("/posts", async (req, res, next) => {
   try {
     const posts = await getAllPosts();
@@ -19,6 +52,45 @@ router.get("/posts", async (req, res, next) => {
   }
 });
 
+/**
+ * @swagger
+ * /api/posts:
+ *   post:
+ *     summary: "게시글 작성"
+ *     description: "서버에 글 데이터를 보내 Post방식으로 저장 요청"
+ *     tags: [Post]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - title
+ *               - author
+ *               - content
+ *             properties:
+ *               title:
+ *                 type: string
+ *                 example: 제목입니다.
+ *               author:
+ *                 type: int
+ *                 example: 1
+ *               content:
+ *                 type: string
+ *                 example: 게시글 내용입니다.
+ *     responses:
+ *       201:
+ *         description: 성공 메시지
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                   message:
+ *                     type: string
+ *                     example : "게시글이 저장되었습니다."
+ */
 router.post("/posts", async (req, res, next) => {
   try {
     const { title, author, content } = req.body;
@@ -34,6 +106,37 @@ router.post("/posts", async (req, res, next) => {
   }
 });
 
+/**
+ * @swagger
+ * /api/posts/:postId:
+ *   get:
+ *     summary: "특정 게시글 조회"
+ *     description: "서버에 Get 방식으로 특정 게시글 조회 요청"
+ *     tags: [Post]
+ *     responses:
+ *       200:
+ *         description: 성공 메시지
+ *         content:
+ *           application/json:
+ *             schema:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: number
+ *                     example: 1
+ *                   title:
+ *                     type: string
+ *                     example: 첫 번째 게시글
+ *                   name:
+ *                     type: string
+ *                     example: cool_user
+ *                   content:
+ *                     type: string
+ *                     example: 게시글 내용입니다.
+ *                   time:
+ *                     type: string
+ *                     example: 2025-05-15T15:23:05.000Z
+ */
 router.get("/posts/:postId", async (req, res, next) => {
   try {
     const postId = req.params.postId;
@@ -46,6 +149,45 @@ router.get("/posts/:postId", async (req, res, next) => {
   }
 });
 
+/**
+ * @swagger
+ * /api/posts/:postId:
+ *   put:
+ *     summary: "게시글 수정"
+ *     description: "서버에 글 데이터를 보내 Put방식으로 수정 요청"
+ *     tags: [Post]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - title
+ *               - author
+ *               - content
+ *             properties:
+ *               title:
+ *                 type: string
+ *                 example: 수정된 제목입니다.
+ *               author:
+ *                 type: int
+ *                 example: 1
+ *               content:
+ *                 type: string
+ *                 example: 수정된 게시글 내용입니다.
+ *     responses:
+ *       200:
+ *         description: 성공 메시지
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                   message:
+ *                     type: string
+ *                     example : "게시글이 수정되었습니다."
+ */
 router.put("/posts/:postId", async (req, res, next) => {
   try {
     const postId = req.params.postId;
@@ -62,6 +204,37 @@ router.put("/posts/:postId", async (req, res, next) => {
   }
 });
 
+/**
+ * @swagger
+ * /api/posts/:postId:
+ *   delete:
+ *     summary: "게시글 삭제"
+ *     description: "서버에 Delete방식으로 삭제 요청"
+ *     tags: [Post]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - author
+ *             properties:
+ *               author:
+ *                 type: int
+ *                 example: 1
+ *     responses:
+ *       200:
+ *         description: 성공 메시지
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                   message:
+ *                     type: string
+ *                     example : "게시글이 삭제되었습니다."
+ */
 router.delete("/posts/:postId", async (req, res, next) => {
   try {
     const postId = req.params.postId;
