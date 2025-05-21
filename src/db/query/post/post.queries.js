029ed1a93d91d69aa29468aa04e4cc97b100f169
yoon-H <ucando0320@gmail.com>
2025-05-22 03:27:44 +0900

    feat : get user id when get post

--- src/db/query/post/post.queries.js
+++ src/db/query/post/post.queries.js
@@ -3,7 +3,7 @@ const SQL_POST_QUERIES = {
     "SELECT p.id AS id, p.title AS title, u.name AS author, p.content AS content, p.create_dt AS time FROM posts AS p LEFT JOIN users AS u ON p.author = u.id;",
   CREATE_POST: "INSERT INTO posts (title, author, content) VALUES (?,?,?)",
   GET_POST:
-    "SELECT p.id AS id, p.title AS title, u.name AS author, p.content AS content, p.create_dt AS time FROM posts AS p LEFT JOIN users AS u ON p.author = u.id WHERE p.id = ?;",
+    "SELECT p.id AS id, p.title AS title, u.id AS userId, u.name AS author, p.content AS content, p.create_dt AS time FROM posts AS p LEFT JOIN users AS u ON p.author = u.id WHERE p.id = ?;",
   EDIT_POST:
     "UPDATE posts SET title = ?, content = ? WHERE id = ? AND author = ?",
   DELETE_POST: "DELETE FROM posts WHERE id = ? AND author =?",
