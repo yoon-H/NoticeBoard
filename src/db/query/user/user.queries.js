82e6d433f27742ad5f8f78d21656cedd55aebfd7
yoon-H <ucando0320@gmail.com>
2025-05-21 20:38:23 +0900

    feat : add function that find user by id

--- src/db/query/user/user.queries.js
+++ src/db/query/user/user.queries.js
@@ -1,6 +1,9 @@
 const SQL_USER_QUERIES = {
-  FIND_USER_BY_LOGIN_ID: 'SELECT id, name, password FROM users WHERE login_id = ?',
-  CREATE_USER: 'INSERT INTO users (name, login_id, password) VALUES (?,?,?)',
+  FIND_USER_BY_LOGIN_ID:
+    "SELECT id, name, password FROM users WHERE login_id = ?",
+  FIND_USER_BY_ID:
+    "SELECT id, name, login_id, password FROM users WHERE id = ?",
+  CREATE_USER: "INSERT INTO users (name, login_id, password) VALUES (?,?,?)",
 };
 
 export default SQL_USER_QUERIES;
