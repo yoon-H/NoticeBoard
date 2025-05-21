dc6e99e6b0b28f37af12a92f403f4ce44d6082e0
yoon-H <ucando0320@gmail.com>
2025-05-21 19:49:04 +0900

    feat : add public files

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ const PORT = config.server.port;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.use("/api", [authRoutes, postRoutes, commentRoutes]);
 
