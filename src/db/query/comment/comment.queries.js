4ccad0d6c09e79bcac85dabc834dd7d6d3706617
yoon-H <ucando0320@gmail.com>
2025-05-28 19:43:55 +0900

    feat : add get comment query

--- src/db/query/comment/comment.queries.js
+++ src/db/query/comment/comment.queries.js
@@ -3,6 +3,8 @@ const SQL_COMMENT_QUERIES = {
     "SELECT c.id AS id, u.name AS author, u.id AS authorId, c.content AS content, c.create_dt AS createTime, c.update_dt AS updateTime FROM comments AS c LEFT JOIN users AS u ON c.author = u.id WHERE c.post_id = ? AND c.is_deleted = FALSE",
   CREATE_COMMENT:
     "INSERT INTO comments (author, content, post_id) VALUES (?,?,?)",
+  GET_COMMENT:
+    "SELECT c.id AS id, c.post_id AS postId, author AS authorId, is_deleted AS isDeleted FROM comments WHERE c.id = ?",
   EDIT_COMMENT: "UPDATE comments SET content = ? WHERE id = ? AND author = ?",
   DELETE_COMMENT:
     "UPDATE comments SET is_deleted = TRUE WHERE id = ? AND author = ?",
