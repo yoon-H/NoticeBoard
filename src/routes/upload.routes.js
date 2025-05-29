650d544913433b533a4caa039ca5a457b150c97e
yoon-H <ucando0320@gmail.com>
2025-05-30 03:33:31 +0900

    feat : add upload attachment api

--- src/routes/upload.routes.js
+++ src/routes/upload.routes.js
@@ -1,14 +1,18 @@
 import { Router } from "express";
-import multerMiddleware from "../middlewares/multer.middleware.js";
+import {
+  multerAttachment,
+  multerImage,
+} from "../middlewares/multer.middleware.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 import { saveTempImage } from "../db/query/image/image.db.js";
+import { saveTempAttachment } from "../db/query/attachment/attachment.db.js";
 
 const router = Router();
 
 router.post(
   "/upload/image",
   authMiddleware,
-  multerMiddleware,
+  multerImage,
   async (req, res, next) => {
     try {
       if (!req.file) {
@@ -32,6 +36,40 @@ router.post(
       await saveTempImage(obj);
 
       return res.status(200).json({ imageUrl: url });
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.post(
+  "/upload/attachment",
+  authMiddleware,
+  multerAttachment,
+  async (req, res, next) => {
+    try {
+      if (!req.file) {
+        return res.status(400).json({ error: "파일이 업로드되지 않았습니다." });
+      }
+
+      const date = new Date();
+      const year = date.getFullYear();
+      const month = String(date.getMonth() + 1).padStart(2, "0");
+      const day = String(date.getDate()).padStart(2, "0");
+
+      const attachmentUrl = `/uploads/attachments/${req.user.id}/${year}/${month}/${day}/`;
+      const url = attachmentUrl + req.file.filename;
+
+      const obj = {
+        userId: req.user.id,
+        originalName: req.file.originalname,
+        storedName: req.file.filename,
+        url: url,
+      };
+
+      await saveTempAttachment(obj);
+
+      return res.status(200).json({ attachmentUrl: url });
     } catch (err) {
       next(err);
     }
