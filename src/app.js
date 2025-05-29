f7994dab1c4e3de7e665c75a9a73343a5270c307
yoon-H <ucando0320@gmail.com>
2025-05-29 14:20:39 +0900

    feat : add uploadRoutes, fix static path

--- src/app.js
+++ src/app.js
@@ -7,7 +7,15 @@ import errorHandlerMiddleware from "./middlewares/error-handler.middleware.js";
 import authRoutes from "./routes/auth.routes.js";
 import postRoutes from "./routes/post.routes.js";
 import commentRoutes from "./routes/comment.routes.js";
+import uploadRoutes from "./routes/upload.routes.js";
 import { specs, swaggerUi } from "./swagger/swagger.js";
+
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const HOST = config.server.host;
@@ -16,9 +24,9 @@ const PORT = config.server.port;
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "../public")));
 
-app.use("/api", [authRoutes, postRoutes, commentRoutes]);
+app.use("/api", [authRoutes, postRoutes, commentRoutes, uploadRoutes]);
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
