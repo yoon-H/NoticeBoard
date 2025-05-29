e3afbb18b5b62e885ea8d3c7b65b9604806f4c2f
yoon-H <ucando0320@gmail.com>
2025-05-29 14:19:19 +0900

    add : multer.middleware.js file

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,38 @@
+import multer from "multer";
+import { v4 as uuidv4 } from "uuid";
+
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const storage = multer.diskStorage({
+  destination: function (req, file, done) {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+
+    const uploadPath = path.join(
+      __dirname,
+      `../../public/uploads/images/${year}/${month}/${day}`
+    );
+
+    fs.mkdirSync(uploadPath, { recursive: true });
+
+    done(null, uploadPath);
+  },
+  filename: function (req, file, done) {
+    const ext = path.extname(file.originalname);
+    const name = uuidv4() + ext;
+
+    done(null, name);
+  },
+});
+
+const multerMiddleware = multer({ storage: storage }).single("image");
+
+export default multerMiddleware;
