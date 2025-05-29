cf486a9f34e9f2b7f822bbb535b605d95c127679
yoon-H <ucando0320@gmail.com>
2025-05-30 02:29:06 +0900

    fix : fix query name

--- src/db/query/image/image.queries.js
+++ src/db/query/image/image.queries.js
@@ -1,5 +1,5 @@
 const SQL_IMAGE_QUERIES = {
-  GET_IMAGES_BY_POSTS:
+  GET_IMAGES_BY_POST:
     "SELECT id, stored_name AS name, url FROM images WHERE post_id = ? AND is_deleted = FALSE",
   GET_TEMP_IMAGES:
     "SELECT id, stored_name AS name, url FROM images WHERE post_id IS NULL AND user_id = ?",
@@ -7,8 +7,8 @@ const SQL_IMAGE_QUERIES = {
     "INSERT INTO images (user_id, stored_name, url) VALUES (?,?,?)",
   SAVE_IMAGE: "UPDATE images SET post_id = ?, is_temp = FALSE WHERE id = ?",
   DELETE_TEMP_IMAGE: "DELETE FROM images WHERE id = ? AND is_temp = TRUE",
-  SOFT_DELETE_IMAGES:
-    "UPDATE images SET is_deleted = TRUE, delete_dt = CURRENT_TIMESTAMP WHERE post_id = ?",
+  SOFT_DELETE_IMAGE:
+    "UPDATE images SET is_deleted = TRUE, delete_dt = CURRENT_TIMESTAMP WHERE id = ?",
 };
 
 export default SQL_IMAGE_QUERIES;
