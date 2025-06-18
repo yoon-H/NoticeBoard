fa795463fd1d5b6ee9548d5adffade45fd064cfb
yoon-H <ucando0320@gmail.com>
2025-06-18 14:41:10 +0900

    add : vite.config.js file

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  root: "react-client", // React 앱의 루트 경로 지정
+  plugins: [react()],
+  build: {
+    outDir: "../dist", // 빌드 결과물 위치 (서버가 서빙할 곳)
+    emptyOutDir: true,
+  },
+  server: {
+    port: 3000,
+  },
+});
