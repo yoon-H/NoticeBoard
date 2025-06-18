c25ffce9a4c5682d9576563faba085fafdb5cf06
yoon-H <ucando0320@gmail.com>
2025-06-18 17:24:19 +0900

    add : status file

---
+++ react-client/components/Status.jsx
@@ -0,0 +1,23 @@
+import { NavLink } from "react-router-dom";
+
+export default function Status(isLoggedIn) {
+  console.log("status");
+
+  let btn1;
+  let btn2;
+
+  if (isLoggedIn) {
+    btn1 = <NavLink to="/sign-up">회원가입</NavLink>;
+    btn2 = <NavLink to="/login">로그인</NavLink>;
+  } else {
+    btn1 = <NavLink to="/mypage">마이페이지</NavLink>;
+    btn2 = <NavLink to="/">로그아웃</NavLink>;
+  }
+
+  return (
+    <>
+      <li>{btn1}</li>
+      <li>{btn2}</li>
+    </>
+  );
+}
