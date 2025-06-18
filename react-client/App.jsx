9c91bd1eb99468c4471e27b173bf5560e9ce1d29
yoon-H <ucando0320@gmail.com>
2025-06-18 17:23:58 +0900

    add : add basic jsx files

---
+++ react-client/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Detail from "./pages/Detail.jsx";
+import Post from "./pages/Post.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import Login from "./pages/Login.jsx";
+import Nav from "./components/Nav.jsx";
+
+export default function App() {
+  console.log("app");
+
+  return (
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        {/* <Route path="/mypage" element={<Mypage/>} /> TODO */}
+        <Route path="/detail" element={<Detail />} />
+        <Route path="/post" element={<Post />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
