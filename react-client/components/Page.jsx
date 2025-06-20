04505c10dd308295e4f2b088e961550685b12ffd
yoon-H <ucando0320@gmail.com>
2025-06-20 11:21:57 +0900

    add : Page.jsx file

---
+++ react-client/components/Page.jsx
@@ -0,0 +1,59 @@
+import { useNavigate } from "react-router-dom";
+
+export default function Page({ totalPages, currentPage }) {
+  const navigate = useNavigate();
+
+  const goToPage = (page) => {
+    navigate(`/?page=${page}`);
+  };
+
+  // 페이지 번호 모음
+  const getPagination = (current, total, delta = 1) => {
+    const result = [];
+
+    const left = Math.max(2, current - delta);
+    const right = Math.min(total - delta, current + delta);
+
+    result.push(1);
+
+    if (left > 2) result.push("...");
+
+    for (let i = left; i <= right; i++) {
+      result.push(i);
+    }
+
+    if (right < total - 1) result.push("...");
+
+    if (total > 1) result.push(total);
+
+    return result;
+  };
+
+  const result = getPagination(currentPage, totalPages);
+
+  // 페이지 넣기
+
+  const pages = result.map((item, idx) => {
+    let element;
+
+    if (item === "...") {
+      element = <span key={idx}>...</span>;
+    } else {
+      element = (
+        <div className="page" key={idx}>
+          <button
+            type="button"
+            className="page-btn"
+            onClick={() => goToPage(item)}
+          >
+            {item}
+          </button>
+        </div>
+      );
+    }
+
+    return element;
+  });
+
+  return <div className="pages">{pages}</div>;
+}
