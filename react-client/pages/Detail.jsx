389bd7e299c64540af4a2fcda79d680da5e93334
yoon-H <ucando0320@gmail.com>
2025-06-20 11:25:39 +0900

    feat : link detail id page

--- react-client/pages/Detail.jsx
+++ react-client/pages/Detail.jsx
@@ -1,7 +1,12 @@
+import { useParams } from "react-router-dom";
+
 export default function Detail() {
+  const params = useParams();
+  const postId = params.id;
     return (
     <>
       <div>Detaaiaiaiaiaial</div>
+      <div>{postId}번 게시물 </div>
     </>
   );
 }
