135adf5838f729ab629c8e4848f2b563d26391d2
yoon-H <ucando0320@gmail.com>
2025-06-23 17:36:21 +0900

    feat : add signup page

--- react-client/pages/SignUp.jsx
+++ react-client/pages/SignUp.jsx
@@ -1,7 +1,102 @@
+import { useState } from "react";
+import "../css/signup.css";
+import { useNavigate } from "react-router-dom";
+import { ID_REG, NAME_REG, PW_REG } from "../utils/validation.js";
+import axios from "axios";
+
+const infos = {
+  id: "",
+  name: "",
+  password: "",
+  confirmPassword: "",
+};
+
 export default function SignUp() {
-    return (
+  const [inputs, setInputs] = useState(infos);
+  const navigate = useNavigate();
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const submitForm = async () => {
+    if (!ID_REG.test(inputs.id)) return alert("아이디를 다시 입력해주세요.");
+
+    if (!NAME_REG.test(inputs.name))
+      return alert("닉네임을 다시 입력해주세요.");
+
+    if (!PW_REG.test(inputs.password))
+      return alert("비밀번호를 다시 입력해주세요.");
+
+    if (inputs.password !== inputs.confirmPassword)
+      return alert("비밀번호와 일치하지 않습니다.");
+
+    try {
+      const res = await axios.post("/api/auth/signup", inputs);
+
+      if (res) navigate("/login");
+    } catch (err) {
+      console.log(err);
+      alert(err.response.data.message);
+    }
+  };
+
+  return (
     <>
-      <div>SignUp</div>
+      <div id="signup-container">
+        <div id="signup-header">
+          <div className="input-text">
+            <p>아이디 </p>
+            <input
+              type="text"
+              name="id"
+              placeholder="아이디"
+              value={inputs.id}
+              onChange={onChange}
+            />
+          </div>
+          <div className="input-text">
+            <p>닉네임 </p>
+            <input
+              type="text"
+              name="name"
+              placeholder="닉네임"
+              value={inputs.name}
+              onChange={onChange}
+            />
+          </div>
+          <div className="input-text">
+            <p>비밀번호 </p>
+            <input
+              type="password"
+              name="password"
+              placeholder="비밀번호"
+              value={inputs.password}
+              onChange={onChange}
+            />
+          </div>
+          <div className="input-text">
+            <p>비밀번호 확인 </p>
+            <input
+              type="password"
+              name="confirmPassword"
+              placeholder="비밀번호 확인"
+              value={inputs.confirmPassword}
+              onChange={onChange}
+            />
+          </div>
+        </div>
+        <div id="submit">
+          <button type="submit" id="submit-btn" onClick={submitForm}>
+            제출
+          </button>
+        </div>
+      </div>
     </>
   );
 }
