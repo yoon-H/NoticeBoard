ecf74af930c985834b4bcf86023bae61d261e41f
yoon-H <ucando0320@gmail.com>
2025-06-26 10:35:38 +0900

    add : axios instance file

---
+++ react-client/utils/axios.instance.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "api",
+});
+
+instance.interceptors.response.use(
+  (res) => res,
+  async (err) => {
+    const originalRequest = err.config;
+
+    //401 인증 에러
+    if (err.response?.status === 401 && originalRequest.retry === false) {
+      originalRequest.retyr = true;
+
+      try {
+        await getNewAccessToken();
+
+        return instance(originalRequest);
+      } catch (err) {
+        console.log("토큰 재발급 실패");
+
+        
+      }
+    }
+
+    return Promise.reject(err);
+  }
+);
+
+async function getNewAccessToken() {
+  await instance.post("/auth/refresh");
+}
