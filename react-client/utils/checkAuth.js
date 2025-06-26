032d1a8c0c69b7b91be693daef59300aa80d319a
yoon-H <ucando0320@gmail.com>
2025-06-26 15:08:50 +0900

    fix : fix checkUser

--- react-client/utils/checkAuth.js
+++ react-client/utils/checkAuth.js
@@ -1,6 +1,10 @@
-import api from "./api/privateInstance.js";
+import privateApi from "./api/privateInstance.js";
+import optionaApi from "./api/optionalnstance.js";
 
-export async function checkUser() {
+export async function checkUser(isPrivate = true) {
+  let api;
+  if (isPrivate) api = privateApi;
+  else api = optionaApi;
   const res = await api.get("/auth/profile");
   return res.data;
 }
