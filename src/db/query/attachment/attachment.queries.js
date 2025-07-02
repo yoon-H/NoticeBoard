65a813df35d2162cbb82da1ee81fdba178a38274
yoon-H <ucando0320@gmail.com>
2025-07-02 16:56:24 +0900

    feat : add file download router

--- src/db/query/attachment/attachment.queries.js
+++ src/db/query/attachment/attachment.queries.js
@@ -1,6 +1,8 @@
 const SQL_ATTACHMENT_QUERIES = {
   GET_ATTACHMENTS_BY_POST:
     "SELECT id, original_name AS originalName, stored_name AS storedName, url FROM attachments WHERE post_id = ? AND is_deleted = FALSE",
+  GET_ATTACHMENT_BY_ID:
+    "SELECT id, original_name AS originalName, stored_name AS storedName, url FROM attachments WHERE id = ? AND is_deleted = FALSE",
   GET_TEMP_ATTACHMENTS:
     "SELECT id, original_name AS originalName, stored_name AS storedName, url FROM attachments WHERE post_id IS NULL AND user_id = ?",
   SAVE_TEMP_ATTACHMENT:
