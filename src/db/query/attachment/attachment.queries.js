436d864bbf3941c6b0b52f61d0b7cccac1b6ec57
yoon-H <ucando0320@gmail.com>
2025-07-02 15:55:54 +0900

    bugfix : soft delete files & images bug

--- src/db/query/attachment/attachment.queries.js
+++ src/db/query/attachment/attachment.queries.js
@@ -5,10 +5,12 @@ const SQL_ATTACHMENT_QUERIES = {
     "SELECT id, original_name AS originalName, stored_name AS storedName, url FROM attachments WHERE post_id IS NULL AND user_id = ?",
   SAVE_TEMP_ATTACHMENT:
     "INSERT INTO attachments (user_id, original_name, stored_name, url) VALUES (?,?,?,?)",
-  SAVE_ATTACHMENT: "UPDATE attachments SET post_id = ?, is_temp = FALSE WHERE id = ?",
-  DELETE_TEMP_ATTACHMENT: "DELETE FROM attachments WHERE id = ? AND is_temp = TRUE",
+  SAVE_ATTACHMENT:
+    "UPDATE attachments SET post_id = ?, is_temp = FALSE WHERE id = ?",
+  DELETE_TEMP_ATTACHMENT:
+    "DELETE FROM attachments WHERE id = ? AND is_temp = TRUE",
   SOFT_DELETE_ATTACHMENT:
-    "UPDATE attachments SET is_deleted = TRUE, delete_dt = CURRENT_TIMESTAMP WHERE id = ?",
+    "UPDATE attachments SET is_deleted = TRUE, delete_dt = CURRENT_TIMESTAMP WHERE post_id = ?",
 };
 
 export default SQL_ATTACHMENT_QUERIES;
