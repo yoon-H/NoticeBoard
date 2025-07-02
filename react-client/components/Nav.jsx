97f23f1e56e0ddc97d56486c18853cc95fb30bb6
yoon-H <ucando0320@gmail.com>
2025-07-02 09:37:17 +0900

    fix : change logged in comment in Nav

--- react-client/components/Nav.jsx
+++ react-client/components/Nav.jsx
@@ -1,5 +1,4 @@
 import { NavLink } from "react-router-dom";
-import { useState } from "react";
 
 import styles from "../css/nav.module.css";
 import { useUser } from "../hooks/useUser.js";
@@ -7,21 +6,19 @@ import { useUser } from "../hooks/useUser.js";
 function Status() {
   const { user } = useUser();
 
-  let btn1;
-  let btn2;
-
-  if (!user) {
-    btn1 = <NavLink to="/sign-up">회원가입</NavLink>;
-    btn2 = <NavLink to="/login">로그인</NavLink>;
-  } else {
-    btn1 = <NavLink to="/mypage">마이페이지</NavLink>;
-    btn2 = <NavLink to="/">로그아웃</NavLink>;
-  }
+  const btn1 = <NavLink to="/sign-up">회원가입</NavLink>;
+  const btn2 = <NavLink to="/login">로그인</NavLink>;
 
   return (
     <>
-      <li>{btn1}</li>
-      <li>{btn2}</li>
+      {!user ? (
+        <>
+          <li>{btn1}</li>
+          <li>{btn2}</li>
+        </>
+      ) : (
+        <p>{user.name}님, 환영합니다.</p>
+      )}
     </>
   );
 }
